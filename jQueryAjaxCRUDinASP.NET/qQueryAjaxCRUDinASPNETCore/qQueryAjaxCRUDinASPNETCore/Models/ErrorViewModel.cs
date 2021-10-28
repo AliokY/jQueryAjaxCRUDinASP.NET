7e239b910c02fbff32fd3b91bfa56c0a353aa529
Юрий Алёхин <aliokhinyury@gmail.com>
2021-10-28 16:46:25 +0300

    add transactionModel

---
+++ jQueryAjaxCRUDinASP.NET/qQueryAjaxCRUDinASPNETCore/qQueryAjaxCRUDinASPNETCore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace qQueryAjaxCRUDinASPNETCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
